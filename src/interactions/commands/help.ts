c37c3521a7392d591f352affb2c9cb75d6d75107
Lunish <luni.siaa@gmail.com>
2022-09-25 22:27:01 +0200

    add help icons & update my name

--- src/interactions/commands/help.ts
+++ src/interactions/commands/help.ts
@@ -7,7 +7,7 @@ export default {
         interaction.reply({
             embeds: [
                 {
-                    description: `**${client.user?.username}**\nWith me you're able to either convert text to .mp3 files for you to download (\`/mp3\`), or to speak messages for you in a voice channel using \`/voice\`. Note that there is a character limit of 300.\n\n**Developers**\n[\`ルナ#8888\`](https://lunish.nl/luna) - Voice Convertion, Bot\n[\`NotFound.sh#3105\`](https://tayronm.tech) - Voice Channel Management`,
+                    description: `**${client.user?.username}**\nWith me you're able to either convert text to .mp3 files for you to download (\`/mp3\`), or to speak messages for you in a voice channel using \`/voice\`. Note that there is a character limit of 300.\n\n**Developers**\n[\`Luny#8888\`](https://lunish.nl/luna) - Voice Convertion, Bot\n[\`NotFound.sh#3105\`](https://tayronm.tech) - Voice Channel Management`,
                     thumbnail: {
                         url: `https://cdn.discordapp.com/avatars/${client.user?.id}/${client.user?.avatar}.${client.user?.avatar?.startsWith('a_') ? 'gif' : 'png'}?size=1024`
                     },
@@ -22,19 +22,22 @@ export default {
                             type: 2,
                             style: 5,
                             label: 'Learn about Waya',
-                            url: `https://waya.one`
+                            url: `https://waya.one`,
+                            emoji: '<a:c_prideblob:932368100832202783>'
                         },
                         {
                             type: 2,
                             style: 5,
                             label: 'Invite Auditional Text',
-                            url: 'https://discord.com/api/oauth2/authorize?client_id=985213199248924722&permissions=3197952&scope=bot%20applications.commands'
+                            url: 'https://discord.com/api/oauth2/authorize?client_id=985213199248924722&permissions=3197952&scope=bot%20applications.commands',
+                            emoji: '<:icons_audioenable:875395222291513354>'
                         },
                         {
                             type: 2,
                             style: 5,
-                            label: 'Vote for us',
-                            url: 'https://top.gg/bot/985213199248924722/vote'
+                            label: 'TikTok',
+                            url: 'https://lunish.nl/tiktok-01',
+                            emoji: '<:Icons_tiktok:941672576449654804>'
                         }
                     ]
                 }

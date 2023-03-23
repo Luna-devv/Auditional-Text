8b31165dc1b2366f46f0c1d680380ebd950549ee
Lunish <luni.siaa@gmail.com>
2023-03-23 18:29:35 +0100

    fix handlers

--- src/handlers/events.ts
+++ src/handlers/events.ts
@@ -5,10 +5,10 @@ import path from 'node:path';
 import { Event } from '../typings';
 
 export default async function (client: Client) {
-    const eventFiles = readdirSync(path.join('..', 'dist', 'events')).filter((file) => file.endsWith('.js'));
+    const eventFiles = readdirSync(`${process.cwd()}/dist/events`).filter((file) => file.endsWith('.js'));
 
     for (const file of eventFiles) {
-        const event: Event = (await import(`../events/${file}`)).default;
+        const event: Event = (await import(`${process.cwd()}/dist/events/${file}`)).default;
         const argumentsFunction = (...args: unknown[]) => event.run(...args);
 
         event.once ? client.once(event.name, argumentsFunction) : client.on(event.name, argumentsFunction);
--- src/handlers/interactions.ts
+++ src/handlers/interactions.ts
@@ -4,10 +4,10 @@ import path from 'node:path';
 import { Config } from '../config';
 
 export default async function () {
-    const commands = readdirSync(path.join('..', 'dist', 'interactions', 'commands')).filter((file) => file.endsWith('.js'));
+    const commands = readdirSync(`${process.cwd()}/dist/interactions/commands`).filter((file) => file.endsWith('.js'));
 
     for (const file of commands) {
-        const pull = (await import(`../interactions/commands/${file}`)).default;
+        const pull = (await import(`${process.cwd()}/dist/interactions/commands/${file}`)).default;
         if (pull.name) Config.data.interactions.commands.set(pull.name, pull);
     }
 }

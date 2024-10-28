19dd9d20708d3c25b8f75af32736b325e95bb61f
Luna <luna@waya.one>
2024-10-28 22:27:03 +0100

    tts generation hot fix

--- src/modules/getData.ts
+++ src/modules/getData.ts
@@ -1,15 +1,13 @@
-import path from 'node:path';
-import fs from 'node:fs';
+import { join } from 'node:path';
+import { writeFile } from 'node:fs/promises';
 
 import { Config } from '../config';
 
 export async function getData(text: string, voice: string): Promise<string | null> {
     const fileName = randomCode(10);
-    const name = path.join(__dirname, '..', 'audio', fileName + '.mp3');
+    const name = join(process.cwd(), 'cache', fileName + '.mp3');
 
-    if (!fs.existsSync(path.join(__dirname, '..', 'audio'))) fs.mkdirSync(path.join(__dirname, '..', 'audio'));
-
-    const data = await fetch(Config.apis.tts + '/api/generation', {
+    const data = await fetch(Config.apis.tts, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -21,7 +19,7 @@ export async function getData(text: string, voice: string): Promise<string | nul
     }).then(async (res) => await res.json()).catch(console.error);
 
     try {
-        fs.writeFileSync(name, Buffer.from(data?.data?.replace?.('data:audio/mp3; codecs=opus;base64,', ''), 'base64'));
+        writeFile(name, Buffer.from(data.data, 'base64'));
         return name;
     } catch (e) {
         console.error(e);
